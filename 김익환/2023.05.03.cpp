7e6b107ddb95839d0737bd623d005c8af59edebd
IkHwan Kim <52392858+dlrghks2090@users.noreply.github.com>
2023-05-03 23:50:53 +0900

    Update and rename test.py to 2023.05.03.cpp
    [프로그래머스_Lv2_연습문제_최댓값과 최솟값]

---
+++ 김익환/2023.05.03.cpp
@@ -0,0 +1,45 @@
+// 프로그래머스 - 레벨2 - 최댓값과 최솟값
+
+// sstream을 사용해 공백을 기준으로 숫자를 추출해 풀이함.
+
+#include <string>
+#include <vector>
+#include <sstream>
+
+using namespace std;
+
+string solution(string s) {
+    string answer = "";
+    
+    stringstream num(s);
+    string tmp;
+    int min, max;
+    int min_flag =0;
+    int max_flag =0;
+    
+    while(num >> tmp) {
+        if (min_flag !=0) {
+            if (stoi(tmp) < min) {
+                min = stoi(tmp);
+            }
+        }
+        else {
+            min = stoi(tmp);
+            min_flag = 1;
+        }
+        
+        if (max_flag!=0) {
+            if (stoi(tmp) > max) {
+                max = stoi(tmp);
+            }
+        }
+        else {
+            max = stoi(tmp);
+            max_flag = 1;
+        }
+    }
+    
+    answer = to_string(min) + " " + to_string(max);
+    
+    return answer;
+}
