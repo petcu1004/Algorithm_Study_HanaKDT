cdc93e1ef259d4b23b7abb30ede532c40c7ec649
Saemi Min <76938304+petcu1004@users.noreply.github.com>
2023-05-07 04:56:21 +0900

    Modify: 인사성 밝은 곰곰이

--- 민새미/05.05.py
+++ 민새미/05.05.py
@@ -1,25 +1,25 @@
 # 인사성 밝은 곰곰이
-## 시간 초과 문제 발생하는 코드
+## 시간 초과 문제를 해시로 해결함.
 
 n=int(input())
 
-m=[]
+dic ={}
 cnt=0
-c=0
-for i in range(n):
+
+for _ in range(n):
     a=input()
-    if a=='ENTER': 
-        m=[]
-    else:
-        
-        if len(m)==0:
-            m.append(a)
-            cnt+=1
-        else:
-            for j in m:
-                if a==j: #똑같은 닉네임이 있다면?
-                    break
-                
-                m.append(a)
+
+    if a=="ENTER":
+        for key, value in dic.items():
+            if value==1:
                 cnt+=1
+        dic={}
+    else:
+        if a not in dic: #중복되는 닉네임이 없을 경우
+            dic[a]=1 #그에 맞는 value값을 1로 설정
+    
+for key, value in dic.items():
+    if value==1:
+        cnt+=1
+
 print(cnt)
