c0453e8767b69f2a80c6f14d09c9c591b556cefa
Saemi Min <76938304+petcu1004@users.noreply.github.com>
2023-05-07 04:35:34 +0900

    Modify: 인사성 밝은 곰곰이

--- 민새미/05.05.py
+++ 민새미/05.05.py
@@ -1,3 +1,6 @@
+# 인사성 밝은 곰곰이
+## 시간 초과 문제 발생하는 코드
+
 n=int(input())
 
 m=[]
@@ -17,9 +20,6 @@ for i in range(n):
                 if a==j: #똑같은 닉네임이 있다면?
                     break
                 
-                print(m)
                 m.append(a)
                 cnt+=1
-
-
 print(cnt)
